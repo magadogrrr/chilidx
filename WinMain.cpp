dc7ec91ef6cf72c72966f0328ca927117ab5c089
torrence payseno <tpayseno@gmail.com>
2019-04-12 22:41:25 -0700

    setting up main window

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,5 +1,18 @@
 #include <Windows.h>
 
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch(msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(69);
+		break;
+	default: ;
+	}
+	return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPreviousInstance,
@@ -11,7 +24,7 @@ int CALLBACK WinMain(
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbWndExtra = 0;
 	wc.cbClsExtra = 0;
 	wc.hInstance = hInstance;
@@ -29,7 +42,12 @@ int CALLBACK WinMain(
 
 	ShowWindow(hWnd, SW_SHOW);
 
-	while (true);
+	MSG msg;
+	while(GetMessage(&msg, nullptr, 0,0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 	
 	return 0;
 }
